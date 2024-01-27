06c33252f49d1b9ecfdd86baa71354f7d8d112ab
Ivan Palamarchuk <ipz222_pis@student.ztu.edu.ua>
2024-01-27 13:24:15 +0200

    create gitignore and .net project

---
+++ SumCounter/Program.cs
@@ -0,0 +1,9 @@
+﻿int a, b;
+if (int.TryParse(Console.ReadLine(), out a) && int.TryParse(Console.ReadLine(), out b))
+{
+    Console.WriteLine($"result: {a + b}");
+}
+else
+{
+    Console.WriteLine("Error with input values");
+}
